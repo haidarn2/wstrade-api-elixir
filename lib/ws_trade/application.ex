94645f586a1a637c5897e0639b82e480e88a2106
Nadeem Haidar <nadeem_haidar@hotmail.com>
2021-05-27 00:45:25 -0400

    add application

---
+++ lib/ws_trade/application.ex
@@ -0,0 +1,19 @@
+defmodule WsTrade.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: WsTrade.Worker.start_link(arg)
+      # {WsTrade.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: WsTrade.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
