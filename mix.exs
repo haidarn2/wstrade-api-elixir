5f7bf62cd40f336593bbb077ef3bc959f15d3fa0
Nadeem Haidar <nadeem_haidar@hotmail.com>
2021-05-28 01:17:41 -0400

    add mix package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule WsTrade.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -28,6 +29,15 @@ defmodule WsTrade.MixProject do
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/haidarn2/wstrade-api-elixir"}
     ]
   end
 end
